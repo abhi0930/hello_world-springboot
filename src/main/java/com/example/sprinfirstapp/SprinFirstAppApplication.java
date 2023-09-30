7db7ac650b3aaf88f814bbaeab08c33849bd50aa
Abhishek <abhishek.agarwal_cs18@gla.ac.in>
2023-09-30 14:54:13 +0530

    adding files

---
+++ src/main/java/com/example/sprinfirstapp/SprinFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinFirstAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprinfirstapp/SprinFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
